f5d102f83bad186bd3d9fc604f2605c16478deb1
mohith <mohithb987@gmail.com>
2024-06-24 07:43:39 -0400

    jenkins installer minor update

--- jenkins-runner-script/jenkins-installer.sh
+++ jenkins-runner-script/jenkins-installer.sh
@@ -8,18 +8,22 @@ sleep 30
 # Jenkins Installation instructions from https://www.jenkins.io/doc/book/installing/linux/
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+sleep 20
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
+sleep 20
 sudo apt-get update
 yes | sudo apt-get install jenkins
 
-sudo systemctl enable jenkins
-sudo systemctl start jenkins
-sudo systemctl status jenkins
+echo "Sleeping for 30 seconds to wait until Jenkins is enabled..."
+sleep 60
 
-sleep 30
-echo "Sleeping for 30 seconds to wait until Jenkins installation is complete..."
+sudo systemctl enable jenkins
+sleep 10
+sudo systemctl start jenkins
+sleep 10
+sudo systemctl status jenkins
 
 
 # Terraform Installation instructions from https://developer.hashicorp.com/terraform/tutorials/aws-get-started/install-cli
